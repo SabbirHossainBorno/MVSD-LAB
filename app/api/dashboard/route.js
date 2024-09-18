9f6a9c1c8fb21fa6a14f9c4e9dd9bc4594612055
root <root@srv563309.hstgr.cloud>
2024-09-19 03:30:01 +0600

    Automated commit: Thu Sep 19 03:30:01 AM +06 2024

--- app/api/dashboard/route.js
+++ app/api/dashboard/route.js
@@ -51,7 +51,7 @@ export async function GET(request) {
   if (diff > 10 * 60 * 1000) { // 10 minutes
     log(`Session expired for email: ${email}`, sessionId, { ip, userAgent });
     await sendTelegramAlert(`MVSD LAB DASHBOARD\n-------------------------------------\nSession Expired.\nPlease Login Again.\nEmail : ${email}`);
-    return NextResponse.json({ message: 'Session Expired.' }, { status: 401 });
+    return NextResponse.json({ message: 'Session Expired! Please Login Again!' }, { status: 401 });
   }
 
   const client = await pool.connect();
