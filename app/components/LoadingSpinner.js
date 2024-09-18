9b90de0b9741f816a6dc3f932f441b8ec52f7dfa
root <root@srv563309.hstgr.cloud>
2024-09-18 20:00:01 +0600

    Automated commit: Wed Sep 18 08:00:01 PM +06 2024

--- app/components/LoadingSpinner.js
+++ app/components/LoadingSpinner.js
@@ -1,10 +1,16 @@
-// ../components/LoadingSpinner.js
-
 import React from 'react';
 
 const LoadingSpinner = () => (
-  <div className="flex items-center justify-center min-h-screen">
-    <div className="w-16 h-16 border-4 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+  <div className="relative flex justify-center items-center min-h-screen">
+    {/* Spinner */}
+    <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-gradient-start border-transparent border-t-purple-500 border-b-purple-500"></div>
+    
+    {/* Image */}
+    <img 
+      src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg" 
+      alt="Thinking Avatar" 
+      className="rounded-full h-28 w-28"
+    />
   </div>
 );
 
