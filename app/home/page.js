91e6db132626e014d49ea2b561cba94066cda26c
root <root@srv563309.hstgr.cloud>
2024-12-09 11:30:01 +0600

    Automated commit: Mon Dec  9 11:30:01 AM +06 2024

--- app/home/page.js
+++ app/home/page.js
@@ -329,19 +329,56 @@ export default function Home() {
       </div>
     </section>
 
-    <div className="wow fadeInUp text-center" style={{ visibility: "visible" }}>
-  <span className="hero-subtitle-gradient relative mb-4 font-medium text-sm inline-flex items-center gap-2 py-2 px-4 rounded-full">
-    <span className="hero-subtitle-text">Some of Main Features</span>
-  </span>
-  <h2 className="text-white mb-4 text-2xl font-extrabold sm:text-4xl xl:text-heading-2">
-    Key Features of Our Tool
-  </h2>
-  <p className="max-w-[714px] mx-auto mb-5 font-medium">
-    Our AI writing tool is designed to empower you with exceptional writing
-    capabilities, making the writing process more efficient, accurate, and
-    enjoyable.
-  </p>
-</div>
+
+    {/* ----------------------------------------------------FEATURES------------------------------------------------------------------ */}
+    <section id="about" className="pt-16 md:pt-20 lg:pt-28">
+      <div className="container">
+        <div className="border-b border-body-color/[.15] pb-16 dark:border-white/[.15] md:pb-20 lg:pb-28">
+          <div className="-mx-4 flex flex-wrap items-center">
+            <div className="w-full px-4 lg:w-1/2">
+              <SectionTitle
+                title="Crafted for Startup, SaaS and Business Sites."
+                paragraph="The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel discussions and roundtables."
+                mb="44px"
+              />
+
+              <div
+                className="wow fadeInUp mb-12 max-w-[570px] lg:mb-0"
+                data-wow-delay=".15s"
+              >
+                <div className="mx-[-12px] flex flex-wrap">
+                  <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
+                    <List text="Premium quality" />
+                    <List text="Tailwind CSS" />
+                    <List text="Use for lifetime" />
+                  </div>
+
+                  <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
+                    <List text="Next.js" />
+                    <List text="Rich documentation" />
+                    <List text="Developer friendly" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full px-4 lg:w-1/2">
+              <div
+                className="wow fadeInUp relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0"
+                data-wow-delay=".2s"
+              >
+                <Image
+                  src="/images/features.png"
+                  alt="about-image"
+                  fill
+                  className="drop-shadow-three mx-auto max-w-full dark:hidden dark:drop-shadow-none lg:mr-0"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
 
         
