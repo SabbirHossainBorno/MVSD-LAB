426d4e16715bf83ec4896f22cb354a7b8ddad61f
root <root@srv563309.hstgr.cloud>
2024-12-15 18:30:02 +0600

    Automated commit: Sun Dec 15 06:30:02 PM +06 2024

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -44,26 +44,31 @@ export default function Navbar() {
 
         {/* Right Section */}
         <div className="flex md:order-2">
-        <Link href="/login">
-  <button
-    type="button"
-    className="relative inline-flex items-center justify-center px-6 py-2 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
-  >
-    {isMobileNavActive ? (
-      "Login"
-    ) : (
-      <>
-        <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
-        <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-        <span className="relative flex items-center">
-          <img src="/images/login.png" alt="Login Icon" className="w-5 h-5 mr-2" />
-          Login
-        </span>
-      </>
-    )}
-  </button>
-</Link>
-
+        <Link
+          href="/login"
+          className="relative inline-flex items-center justify-center p-2 px-3 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded shadow-md group"
+        >
+          <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              />
+            </svg>
+          </span>
+          <span className="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+            Login
+          </span>
+          <span className="relative invisible">Login</span>
+        </Link>
 
           {/* Mobile menu button */}
           <button
