934908082c2fb8be14a9eb383e4b225210518b03
root <root@srv563309.hstgr.cloud>
2024-12-23 13:00:02 +0600

    Automated commit: Mon Dec 23 01:00:02 PM +06 2024

--- app/login/page.js
+++ app/login/page.js
@@ -132,14 +132,14 @@ function LoginPage() {
                   </div>
                 </div>
                 <div className="mt-4 flex items-center justify-center gap-x-2">
-  <button
-    type="submit"
-    className="relative rounded px-8 py-3 overflow-hidden group bg-blue-800 hover:bg-gradient-to-r hover:from-blue-800 hover:to-blue-600 text-white hover:ring-2 hover:ring-offset-2 hover:ring-blue-500 transition-all ease-out duration-300 w-full max-w-[200px]"
-  >
-    <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-    <span className="relative text-lg md:text-xl transition-transform duration-300 ease-in-out group-hover:scale-110">Login</span>
-  </button>
-</div>
+                  <button
+                    type="submit"
+                    className="relative rounded px-8 py-3 overflow-hidden group bg-blue-800 hover:bg-gradient-to-r hover:from-blue-800 hover:to-blue-600 text-white hover:ring-2 hover:ring-offset-2 hover:ring-blue-500 transition-all ease-out duration-300 w-full max-w-[200px]"
+                  >
+                    <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+                    <span className="relative text-lg text-bold md:text-xl transition-transform duration-300 ease-in-out group-hover:scale-110">Login</span>
+                  </button>
+                </div>
               </form>
             </div>
           </div>
